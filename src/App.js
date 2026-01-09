657a98884bb076a4e41a42c864fef31850986210
Mohamed Khaled <mokhaled832001@gmail.com>
2026-01-10 00:32:03 +0200

    Update project files

--- src/App.js
+++ src/App.js
@@ -7,28 +7,17 @@ import CartTwo from "./CartTwo";
 function App() {
   return (
     <div className="App">
-      <div style={{ margin: "0 70px" }}>
+      <div>
         <Header />
       </div>
-      <div
-        style={{
-          display:"flex",
-          margin: "50px 150px",
-          /*backgroundColor: "red",*/
-          gap:"90px",
-         
-          width:"980px",
-          height:"550px",
-          padding:"30px"
-        }}
+      <div className="Body"
       >
         <CartOne />
         <CartTwo />
       </div>
-
-
     </div>
   );
 }
 
 export default App;
+
